5fcb6c1d5f1ca08d541b98b8325d890e1b08129c
Michael Anderson <mganderson87@gmail.com>
2017-02-10 10:51:09 -0500

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,3 +1,5 @@
+# This file is not used and should be deleted / code included/refined in markov_bot.py
+
 import praw
 
 cs288_post_IDs = ["5moshs", "5k08py", "3r8xe8", "3hzzu5", "5omhuq", "5q72iu", "5ouemn", "4d4xh5", "5mrdhq", "5oww9h", "5iq7i5", "46zd27", "4a05xw"]
